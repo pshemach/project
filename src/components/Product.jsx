340cbc95a5fe3f08ea0a4e23a3b5666b935c20eb
Pasindu <pasindusandakan@gmail.com>
2024-03-13 09:53:40 +0530

    fetch from json

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Product() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8000/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div>
       <div>products</div>
