306a1eb0eebc79c49cadf7e389ce86f8325f0d66
Pasindu <pasindusandakan@gmail.com>
2024-03-13 09:29:19 +0530

    route updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Product from "./components/Product";
+import Error from "./components/Error";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
